a883b0be8ed697c536df5c27e56a884ad91263ca
Martin Maly <mmaly@google.com>
2023-08-29 12:57:57 -0700

    Print specialization progress

--- scripts/specialize.sh
+++ scripts/specialize.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 while IFS="=" read -r key value; do
-    grep -rl '\$\$'"$key"'\$\$' ./* | xargs sed -i 's/\$\$'"$key"'\$\$/'"$value"'/g'
+  echo "Replacing ${key} --> ${value}"
+  grep -rl '\$\$'"$key"'\$\$' ./* | xargs sed -i 's/\$\$'"$key"'\$\$/'"$value"'/g'
 done <specialize.vars
