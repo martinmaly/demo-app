270f58d7dfe682ce9da70d55146c78a21ac9e263
Martin Maly <mmaly@google.com>
2023-08-29 14:11:37 -0700

    Run xargs in verbose mode

--- scripts/specialize.sh
+++ scripts/specialize.sh
@@ -3,7 +3,7 @@
 while IFS="=" read -r key value; do
   if [[ -n "${key}" ]]; then
     echo "Replacing ${key} --> ${value}"
-    grep -rl '\$\$'"$key"'\$\$' ./* | xargs --no-run-if-empty sed -i 's/\$\$'"$key"'\$\$/'"$value"'/g'
+    grep -rl '\$\$'"$key"'\$\$' ./* | xargs --verbose --no-run-if-empty sed -i 's/\$\$'"$key"'\$\$/'"$value"'/g'
   fi
 done <specialize.vars
 
